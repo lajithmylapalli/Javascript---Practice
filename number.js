888d47423c827bcb44060c67265624a695986018
Lajith Mylapalli <147474798+lajithmylapalli@users.noreply.github.com>
2025-10-25 22:48:53 +0530

    Add files via upload

---
+++ number.js
@@ -0,0 +1,17 @@
+let x = 45;
+const y = 36;
+
+var sum = x+y; // addition
+console.log(sum);
+
+var mul = x*y; // multiplicatpion
+console.log(mul);
+
+var div = x/y;
+console.log(div); // division
+
+var sub = x-y; // subtraction
+console.log(sub);
+
+const rem = x%y; //reminder
+console.log(rem);
