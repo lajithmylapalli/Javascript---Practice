7ff92899bcc824c676f1a869748d76cb0566f553
Lajith Mylapalli <147474798+lajithmylapalli@users.noreply.github.com>
2025-10-25 22:48:19 +0530

    Add files via upload

---
+++ boolean.js
@@ -0,0 +1,18 @@
+let x = true;
+let y = false;
+if(x ){
+    console.log(10);
+}
+else{
+    console.log(25);
+}
+
+let a = 56;
+let b = 45;
+let ans = a>b;
+if(ans){
+    console.log(' X is greater than y');
+}
+else{
+    console.log('Y is greater than x');
+}
