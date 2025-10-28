5cc083cedd47dc779b9acd6ae937d09a5d4b1127
Lajith Mylapalli <147474798+lajithmylapalli@users.noreply.github.com>
2025-10-28 12:10:43 +0530

    Add files via upload

---
+++ largest3.js
@@ -0,0 +1,13 @@
+const num = (x,y,z)=>{
+    if ((x>y) && (x>z)){
+        console.log(`${x} is the largest number`);
+
+    }
+    else if ((y>x) && (y>z)){
+        console.log(`${x} is the largest number`);
+        }
+        else{
+            console.log(`${z} is the largest number`);
+        }
+}
+num(45,67,34)
