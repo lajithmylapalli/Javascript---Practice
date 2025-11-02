ce31f8869cd002b13c45593d78991149aff36509
Lajith Mylapalli <147474798+lajithmylapalli@users.noreply.github.com>
2025-11-02 20:17:20 +0530

    Add files via upload

--- reverse.js
+++ reverse.js
@@ -1,4 +1,24 @@
-const rev=(x)=>{
-    return x.split('').reverse().join('');
-};
-console.log(rev('hello'));
+//Reverse a string (e.g., “hello” → “olleh”).
+let name = "HELLO";
+const rev = ()=>{
+    let reverse = name.split('').reverse().join('');
+    console.log(reverse);
+};rev();
+
+//reverse array
+let arr = [2,3,4,5];
+const revarr = ()=>{
+    let ar = arr.reverse();
+    console.log(ar);
+};revarr();
+
+//sum array
+let s = [1,2,3,4];
+let n=0;
+const sum=()=>{
+    
+    for(let i = 0 ;i<s.length;i++){
+        n+=s[i];
+    }
+    console.log(n);
+};sum();
