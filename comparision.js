58c7cea7a4141ba6505eab589aaedb8826dd0358
Lajith Mylapalli <147474798+lajithmylapalli@users.noreply.github.com>
2025-11-07 20:15:29 +0530

    Add files via upload

---
+++ comparision.js
@@ -0,0 +1,23 @@
+let x = 35;
+let y= 45;
+
+if(x!=y){ //!=
+    console.log('X not eqauals to Y');
+}
+else{
+    console.log('X is Equals to Y');
+}
+
+if(x>y){ //>
+    console.log('X is greater than Y');
+}
+else{
+    console.log('X not equals to Y');
+}
+
+if(x>=y){ //>=
+    console.log('X is greater than or equals Y');
+}
+else{
+    console.log('X less than or not equals to Y');
+}
