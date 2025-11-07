d5d955d6d1b3355a3e1cf576a5610d59762a0cd7
Lajith Mylapalli <147474798+lajithmylapalli@users.noreply.github.com>
2025-11-07 20:15:11 +0530

    Add files via upload

---
+++ logical.js
@@ -0,0 +1,17 @@
+let x= 34;
+let y= 45;
+
+if (x>10 && y>15){ //&&
+    console.log('true');
+}
+else{
+    console.log('false');
+}
+
+if(x<45 || y>99){  //||
+    console.log('hi');
+}
+else{
+    console.log('hello');
+}
+
