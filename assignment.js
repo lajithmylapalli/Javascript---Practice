67897edb1e0d3067e638d36feadc84cbc9016f35
Lajith Mylapalli <147474798+lajithmylapalli@users.noreply.github.com>
2025-11-07 20:15:51 +0530

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,17 @@
+let x=1;
+let y=5;
+let a=2;
+let b=16;
+let c=54;
+
+ x+=5;
+y-=6;
+a*=7;
+ b/=8;
+ c%=7;
+ console.log(x);
+ console.log(y);
+ console.log(a);
+ console.log(b);
+ console.log(c);
+console.log(x+y+a+b+c);
