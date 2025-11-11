994134ff7bbc527ca344b0acbf797143628cd259
Lajith Mylapalli <147474798+lajithmylapalli@users.noreply.github.com>
2025-11-11 18:47:36 +0530

    Add files via upload

---
+++ evenorodd.js
@@ -0,0 +1,13 @@
+//fifth problem
+const even = (num)=>{
+    if(num%2==0){
+        console.log(`${num} is a even number`);
+    }
+    else{
+        console.log(`${num} is a odd number`);
+    }
+};
+even(2);
+even(3);
+even(99);
+even(100);
