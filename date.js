0b738deb371d99e53d3fd04cfbe2f754da68204e
Lajith Mylapalli <147474798+lajithmylapalli@users.noreply.github.com>
2025-11-19 14:33:21 +0530

    Add files via upload

--- date.js
+++ date.js
@@ -1,11 +1,2 @@
-const date = ()=>{
-    let now = new Date();
-    console.log("Year : ",now.getFullYear());
-    console.log("Month : ",now.getMonth()+1);
-    console.log("date : ",now.getDate());
-    console.log("Time : ",now.getTime());
-    console.log("Day : ",now.getDay());
-    console.log("Hours : ",now.getHours());
-    console.log("Minutes : ",now.getMinutes());
-    console.log("Seconds : ",now.getSeconds());
-};date();
+const da = ()=>{const date = new Date();
+console.log(date.getDate());};da();
