3ee3bf3d4dfa8d139cced7c5ef59297c29e7f32e
Lajith Mylapalli <147474798+lajithmylapalli@users.noreply.github.com>
2025-11-27 18:16:44 +0530

    Add files via upload

---
+++ includes.js
@@ -0,0 +1,7 @@
+//includes
+const ind = ()=>{
+    const str = "hello world";
+    console.log(str.includes("hell"));
+    console.log(str.includes("hi"));
+    console.log(str.includes("wro"));
+};ind();
